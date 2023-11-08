78ccba5408c24a93137187794fef1f8a11518f4c
KaranAhuja091 <87354410+KaranAhuja091@users.noreply.github.com>
2023-11-08 02:43:50 -0800

    Update beautify.py

--- beautify.py
+++ beautify.py
@@ -1,12 +1,10 @@
-import streamlit as st
 import fitz  # PyMuPDF
 import io
 
 # Function to add a border and watermark to each page
-def add_border_and_watermark(pdf_file, watermark_text):
-    pdf_document = fitz.open(stream=pdf_file.read(), filetype="pdf")
-    output_buffer = io.BytesIO()
-
+def add_border_and_watermark(pdf_file, output_file, watermark_text):
+    pdf_document = fitz.open(pdf_file)
+    
     for page_num in range(len(pdf_document)):
         page = pdf_document.load_page(page_num)
 
@@ -16,25 +14,16 @@ def add_border_and_watermark(pdf_file, watermark_text):
         # Add a watermark
         page.insert_text(page.MediaBox.width / 2, page.MediaBox.height / 2, watermark_text, fontname="Helvetica", fontsize=40, overlay=True)
 
-    pdf_document.save(output_buffer, garbage=4)
-    output_buffer.seek(0)
+    pdf_document.save(output_file)
+    return output_file
 
-    return output_buffer
+# Provide your input and output file paths
+input_pdf_file = "input.pdf"
+output_pdf_file = "output.pdf"
+watermark_text = "Z-DIVISION"
 
-st.title("PDF Page Border and Watermark")
+# Usage example
+with open(input_pdf_file, "rb") as pdf_file:
+    output_path = add_border_and_watermark(pdf_file, output_pdf_file, watermark_text)
 
-uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
-
-if uploaded_file is not None:
-    st.write("PDF file uploaded!")
-
-    watermark_text = st.text_input("Watermark Text", "Z-DIVISION")
-
-    if st.button("Add Border and Watermark"):
-        try:
-            with io.BytesIO(uploaded_file.read()) as pdf_file:
-                output_buffer = add_border_and_watermark(pdf_file, watermark_text)
-            st.success("Border and watermark added successfully!")
-            st.download_button("Download PDF", output_buffer, key="download_pdf", file_name="output.pdf")
-        except Exception as e:
-            st.error(f"An error occurred: {str(e)}")
+print("PDF with border and watermark saved to", output_path)
